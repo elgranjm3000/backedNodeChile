c78e6bdb97208faff0deb866c214ffb5ad490f09
Joseph Muentes <muentes2@hotmail.com>
2024-05-08 17:33:16 -0400

    register and password

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 const mysql = require('mysql');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const saltRounds = 10; 
 
 
 const app = express();
@@ -35,12 +36,36 @@ db.connect((err) => {
 // Secret key para firmar los tokens
 const secretKey = '48334';
 
+
+function generarHash(password) {
+  return new Promise((resolve, reject) => {
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(hash);
+      }
+    });
+  });
+}
+
 // Ruta de registro de usuarios
 app.post('/api/sign-in', async (req, res) => {
   const { email, password } = req.body;
   let payload = {};
- // Verificar si el usuario ya existe
- db.query('SELECT * FROM psid_employee WHERE email = ?', [email], async (error, results) => {
+
+  
+//$2y$10$uz24GabUM0f8.9/8/hxtPeOUmt9AsF2IQY.4itrPPF3f/9L52ZyKS
+  const password1 = 'admin';
+  try {
+    const hash = await generarHash(password1);
+    console.log('Hash bcrypt:', hash);
+  } catch (err) {
+    console.error('Error al generar el hash:', err);
+  }
+
+  // Verificar si el usuario ya existe
+  db.query('SELECT * FROM psid_employee WHERE email = ?', [email], async (error, results) => {
     if (error) {
       console.error(error);
       return res.status(500).json({ message: 'Error interno del servidor' });
@@ -48,56 +73,94 @@ app.post('/api/sign-in', async (req, res) => {
 
     if (results.length == 0) {
       return res.status(400).json({ message: 'Usuario no registrado' });
-    }else{
-         payload = {
-            data: {
-              displayName: results[0].firstname + " " + results[0].lastname,
-              photoURL: "assets/images/avatars/Abbott.jpg",
-              email: email,
-              settings: {
-                Layout: {
-                  style: ["layout2"],
-                  config: {
-                    mode: "boxed",
-                    scroll: "content"
-                  },
-                  navbar: {
-                    display: true
-                  }
-                },
-                theme: {}
+    } else {
+      // Verificar la contraseña
+      const user = results[0];
+      const isPasswordValid = await bcrypt.compare(password, user.passwd);
+
+      if (!isPasswordValid) {
+        return res.status(400).json({ message: 'Contraseña incorrecta' });
+      }
+
+      payload = {
+        data: {
+          displayName: user.firstname + " " + user.lastname,
+          photoURL: "assets/images/avatars/Abbott.jpg",
+          email: email,
+          settings: {
+            Layout: {
+              style: ["layout2"],
+              config: {
+                mode: "boxed",
+                scroll: "content"
               },
-              shortcuts: [
-                "apps.calendar",
-                "apps.mailbox",
-                "apps.contacts"
-              ]
+              navbar: {
+                display: true
+              }
             },
-            role: 'admin'
-          };
+            theme: {}
+          },
+          shortcuts: [
+            "apps.calendar",
+            "apps.mailbox",
+            "apps.contacts"
+          ]
+        },
+        role: 'admin'
+      };
 
-          const expiresIn = '30m'; // El token expirará en 30 min
+      const expiresIn = '30m'; // El token expirará en 30 min
+      const token = jwt.sign({ payload }, secretKey, { expiresIn });
 
+      const response = {
+        uuid: "token_valid",
+        from: "custom-db",
+        password: isPasswordValid, // No recomendado enviar la contraseña en la respuesta
+        user: payload,
+        access_token: token
+      };
 
-          const token = jwt.sign({ payload }, secretKey, { expiresIn });
+      res.status(200).json(response);
+    }
+  });
+});
 
-          const response = {
-            uuid: "token_valid",
-            from: "custom-db",
-            password: "$2y$10$PNWjp0z3Cc8dRksriddWc.xUnW9/TI1BQkeDu6GVvTn7K4tAFeiCq",
-            user: payload,
-            access_token: token
-          };
-         
-          res.status(200).json(response);
+app.post('/api/register', async (req, res) => {
+  const { email, password, displayName } = req.body;
 
+  // Verificar si el usuario ya está registrado
+  db.query('SELECT * FROM psid_employee WHERE email = ?', [email], async (error, results) => {
+    if (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Error interno del servidor' });
     }
 
+    if (results.length > 0) {
+      return res.status(400).json({ message: 'El usuario ya está registrado' });
+    } else {
+      // Crear un nuevo usuario
+      try {
+        const hash = await generarHash(password);
+        console.log('Hash bcrypt:', hash);
 
+        // Insertar el nuevo usuario en la base de datos
+        db.query('INSERT INTO psid_employee (lastname, email, passwd) VALUES (?, ?, ?)', [displayName,email, hash], (error, results) => {
+          if (error) {
+            console.error(error);
+            return res.status(500).json({ message: 'Error al registrar el usuario' });
+          }
+
+          // Si el registro es exitoso, devolver un mensaje de éxito
+          res.status(200).json({ message: 'Usuario registrado exitosamente' });
+        });
+      } catch (err) {
+        console.error('Error al generar el hash:', err);
+        return res.status(500).json({ message: 'Error al registrar el usuario' });
+      }
+    }
+  });
 });
 
-
-});
 
 // Ruta de inicio de sesión
 app.post('/login', async (req, res) => {
@@ -165,7 +228,7 @@ app.post('/api/list/product', (req, res) => {
 });
 
 // Iniciar el servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3007;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://database:${PORT}`);
 });
