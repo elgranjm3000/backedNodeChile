721db5fbaaa5f1323f4de569f73b5c727c8cf4ab
Joseph Muentes <muentes2@hotmail.com>
2024-05-27 10:50:37 -0400

    modificando banckend

--- index.js
+++ index.js
@@ -993,6 +993,37 @@ app.get('/api/task', async (req, res) => {
 });
 
 
+app.get('/api/task/tag', async (req, res) => {
+  const response =  [
+    {
+      "id": "a0bf42ca-c3a5-47be-8341-b9c0bb8ef270",
+      "title": "Api"
+    },
+    {
+      "id": "c6058d0d-a4b0-4453-986a-9d249ec230b1",
+      "title": "Frontend"
+    },
+    {
+      "id": "d3ef4226-ef2c-43b0-a986-3e3e07f32799",
+      "title": "Bug"
+    },
+    {
+      "id": "51483dd3-cb98-4400-9128-4bd66b455807",
+      "title": "Backend"
+    },
+    {
+      "id": "91658b8a-f382-4b0c-a53f-e9390351c2c5",
+      "title": "Urgent"
+    },
+    {
+      "id": "2b884143-419a-45ca-a7f6-48f99f4e7798",
+      "title": "Discuss"
+    }
+  ];
+
+  res.status(200).json(response);
+
+})
 
 // Ruta de inicio de sesión
 app.post('/login', async (req, res) => {
