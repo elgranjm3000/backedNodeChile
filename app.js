cb14ff123761913f2fe0949c2832d7ecdfc9e811
Joseph Muentes <muentes2@hotmail.com>
2024-08-24 22:40:25 -0400

    get in tag

--- app.js
+++ app.js
@@ -1098,7 +1098,7 @@ app.get('/api/task', async (req, res) => {
 });
 
 
-app.post('/api/task/tag', async (req, res) => {
+get.post('/api/task/tag', async (req, res) => {
 
   const token = req.headers.authorization.split(' ')[1];
   try {
